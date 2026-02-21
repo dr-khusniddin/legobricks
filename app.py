5d65dc57e5521da8c0df340ae867227921d7790a
dr-khusniddin <khrt.frdom@gmail.com>
2026-02-21 13:24:56 +0500

    Fix indentation error at the end of file

--- app.py
+++ app.py
@@ -177,5 +177,5 @@ def submit_order():
     return jsonify({'success': True})
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
